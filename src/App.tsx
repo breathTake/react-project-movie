0c9d8366cd8e4e94d0f0622d5d62f0f16e03132a
breathTake <104220135+breathTake@users.noreply.github.com>
2023-04-24 21:49:37 +0200

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,60 @@
-import logo from './assets/logo.svg';
-import styles from './App.module.scss';
+import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from 'react';
+import { useState, useEffect } from 'react';
 
-function App() {
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+import Classnames from 'classnames';
+
+const API_URL = 'http://www.omdbapi.com/?apikey=a80a3d59';
+
+const movie1 = {
+    Title: 'Star Wars: Episode IV - A New Hope',
+    Year: '1977',
+    imdbID: 'tt0076759',
+    Type: 'movie',
+    Poster: 'https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg',
+};
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title: any) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    };
+
     return (
-        <div className={styles.App}>
-            <header className={styles['App-header']}>
-                <h1>React Movie Project</h1>
-            </header>
+        <div className={Classnames('App', 'app')}>
+            <h1>Jona's Movie Land</h1>
+
+            <div className="search">
+                <input 
+                placeholder="Search..." 
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)} />
+
+                <img src={SearchIcon} 
+                alt="search" 
+                onClick={(e) => searchMovies(searchTerm)} />
+            </div>
+
+            {movies?.length > 0 ? (
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard props={movie} />
+                    ))}
+                </div>
+            ) : (
+                <div className="empty">
+                    <h2>No movies found</h2>
+                </div>
+            )}
         </div>
     );
-}
+};
 
 export default App;
--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -4,4 +4,10 @@ import App from '../../../App';
 export default createBoard({
     name: 'App',
     Board: () => <App />,
+    environmentProps: {
+        canvasHeight: 1054,
+        windowHeight: 839,
+        windowWidth: 1598,
+        canvasWidth: 1440,
+    },
 });
